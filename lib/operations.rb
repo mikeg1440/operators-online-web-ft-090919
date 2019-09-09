33bac66baa2bb69cd70ed905d0f32463d56a5b24
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-09 15:11:34 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,15 @@
 require "pry"
 
 def unsafe?(speed)
-	if speed < 40 || speed > 60
-		binding.pry
-		return true
-	else
-		binding.pry
-		return false
-	end
+	# if speed < 40 || speed > 60
+	# 	binding.pry
+	# 	return true
+	# else
+	# 	binding.pry
+	# 	return false
+	# end
+	speed < 40 || speed > 60 ? true : false
+
 end
 
 
